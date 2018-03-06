9339bd5560af7890fed9a3a9a08f1eb9067467d9
wk-j <somnuk.wk@gmail.com>
2018-03-06 22:51:14 +0700

    Added configurations

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 Add rainbow effect to quoted string
 
 ![](https://raw.githubusercontent.com/wk-j/vscode-rainbow-string/master/images/Rainbow.png)
+
+## Configurations
+
+```json
+    "rainbowString": {
+        "extensions": [
+            ".cs",
+            ".csx",
+            ".fs",
+            ".fsx",
+            ".js",
+            ".fsx",
+            ".ts",
+            ".tsx",
+            ".csproj",
+            ".fsproj",
+            ".cake"
+        ]
+```
--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,24 @@ import { decorate } from './decorator';
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "vscode-rainbow-string" is now active!');
 
+    let config = vscode.workspace.getConfiguration("rainbowString") as any as { extensions: string[] }
+
+    vscode.workspace.onDidChangeConfiguration(() => {
+        config = vscode.workspace.getConfiguration("rainbowString") as any as { extensions: string[] }
+    });
+
     vscode.workspace.onDidSaveTextDocument(e => {
-        decorate()
+        var file = vscode.window.activeTextEditor.document.fileName;
+        if (config.extensions.some(x => file.endsWith(x))) {
+            decorate()
+        }
     })
 
     vscode.workspace.onDidOpenTextDocument(e => {
-        decorate()
+        var file = vscode.window.activeTextEditor.document.fileName;
+        if (config.extensions.some(x => file.endsWith(x))) {
+            decorate()
+        }
     })
 }
 
